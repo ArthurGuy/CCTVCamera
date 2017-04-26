5f032191a3e8c1a1c37ce3df3fc4591af1cca8c1
Arthur Guy <arthur@arthurguy.co.uk>
2017-04-26 23:14:38 +0100

    Update camera.py

--- camera.py
+++ camera.py
@@ -5,6 +5,6 @@ from time import sleep
 camera = picamera.PiCamera()
 
 camera.capture('image.jpg')
-with open('image.jpg', 'rb') as f: r = requests.post('http://requestb.in/qr61r7qr', files={'image.jpg.xls': f})
+with open('image.jpg', 'rb') as f: r = requests.post('https://bbms.buildbrighton.com/camera/store', files={'image.jpg.xls': f})
 
 sleep(30);
