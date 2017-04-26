b27abbeadc149795004797c918aa2a9799ae2027
Arthur Guy <arthur@arthurguy.co.uk>
2017-04-27 00:24:47 +0100

    Update camera.py

--- camera.py
+++ camera.py
@@ -3,8 +3,12 @@ import requests
 from time import sleep
 
 camera = picamera.PiCamera()
+camera.resolution = (1024, 768)
 
+camera.start_preview()
+sleep(2);
 camera.capture('image.jpg')
+
 with open('image.jpg', 'rb') as f: r = requests.post('https://bbms.buildbrighton.com/camera/store', files={'image': f})
 
-sleep(30);
+sleep(2);
